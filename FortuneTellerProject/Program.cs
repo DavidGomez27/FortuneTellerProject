3f6633749895ee1024e32b457070e1007da0001d
DavidGomez27 <mmcsgomez16@gmail.com>
2017-09-29 14:34:43 -0400

    laying foundation

--- FortuneTellerProject/Program.cs
+++ FortuneTellerProject/Program.cs
@@ -10,6 +10,64 @@ namespace FortuneTellerProject
     {
         static void Main(string[] args)
         {
+
+            Console.WriteLine("Hello. I will provide your fortune today based on the answers you provide. Press \"enter\" to continue");
+            Console.ReadLine();
+
+
+            Console.WriteLine("What is your first name?");
+            string firstName = Console.ReadLine();
+           
+
+           
+            Console.WriteLine("What is your last name?");
+            string lasttName = Console.ReadLine();
+
+            
+            Console.WriteLine("How old are you?");
+            int age = int.Parse(Console.ReadLine());
+
+            
+            Console.WriteLine("What is the NUMBER of your birth month?");
+            int birthMonth = int.Parse(Console.ReadLine());
+
+
+
+            Console.WriteLine("What is your favorite ROYGBIV color? Type \"Help\" and press enter for list of colors");
+            string userAnswer = Console.ReadLine();
+            if (userAnswer == "Help")
+            {
+                Console.WriteLine("Red,Orange,Yellow,Green,Blue,Indigo,Violet");
+            }
+
+                    
+
+                  
+      
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 }
