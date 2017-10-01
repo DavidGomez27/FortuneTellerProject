3ce40c3996f6f4a9556b2bd9a26a9a27b95a4f33
DavidGomez27 <mmcsgomez16@gmail.com>
2017-10-01 14:04:00 -0400

    variable making

--- FortuneTellerProject/Program.cs
+++ FortuneTellerProject/Program.cs
@@ -21,29 +21,63 @@ namespace FortuneTellerProject
 
            
             Console.WriteLine("What is your last name?");
-            string lasttName = Console.ReadLine();
+            string lastName = Console.ReadLine();
 
             
             Console.WriteLine("How old are you?");
             int age = int.Parse(Console.ReadLine());
+            if (age % 2 == 0)
+            {
+                age = 50;
+            }
+            else age = 5;
 
-            
+
+
             Console.WriteLine("What is the NUMBER of your birth month?");
             int birthMonth = int.Parse(Console.ReadLine());
+            if (birthMonth >= 1 && birthMonth <= 4)
+            {
+                birthMonth = 1000000;
+            }
+            else if (birthMonth > 4 && birthMonth <= 8)
+            {
+                birthMonth = 20;
+            }
+            else
+            {
+                birthMonth = 65000;
+            }
+
+
+
 
 
 
             Console.WriteLine("What is your favorite ROYGBIV color? Type \"Help\" and press enter for list of colors");
-            string userAnswer = Console.ReadLine();
-            if (userAnswer == "Help")
+            string favoriteColor = Console.ReadLine();
+            if (favoriteColor == "Help")
             {
                 Console.WriteLine("Red,Orange,Yellow,Green,Blue,Indigo,Violet");
             }
+            else favoriteColor = (Console.ReadLine());
 
-                    
 
-                  
-      
+
+
+
+
+            Console.WriteLine("How many siblings do you have?");
+            int numberOfSiblings = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Final chance to turn back. You could be doomed...Press \"enter\" for fortune");
+            Console.ReadLine();
+
+
+            Console.WriteLine(firstName + " " + lastName + " " + "will retire in" + " " + age + "years" + " " + "with" + " " + birthMonth + " " + "dollars in the bank");
+
+
+
 
 
 
